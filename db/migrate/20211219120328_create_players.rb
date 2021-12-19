a7695c0bf9e9e839407f0f6ddbd42872aad7dc02
Satoshi Tanaka <st34.satoshi@gmail.com>
2021-12-19 21:12:38 +0900

    add: player model

---
+++ db/migrate/20211219120328_create_players.rb
@@ -0,0 +1,11 @@
+class CreatePlayers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :players do |t|
+      t.string :name_en
+      t.string :name_jp
+      t.string :rating_standard
+
+      t.timestamps
+    end
+  end
+end
