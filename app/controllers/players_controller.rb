dc0e980616bb874e4252828b01fd28e27da57c7b
Satoshi Tanaka <st34.satoshi@gmail.com>
2021-12-26 16:17:27 +0900

    tmp: able to search using a name

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,6 @@
 class PlayersController < ApplicationController
 
     def index
-        @players = Player.all.order(rating_standard: "DESC").page(params[:page]).per(25)
+        @players = Player.players_name_has(params[:name]).ordered.page(params[:page]).per(25)
     end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,9 @@
 class Player < ApplicationRecord
+    scope :name_en_has, ->(name) { where("name_en like ?", "%#{name}%") }
+    scope :name_jp_has, ->(name) { where("name_jp like ?", "%#{name}%") }
+    scope :ordered, -> { order(rating_standard: "DESC") }
+
+    def self.players_name_has(name)
+        Player.all.name_en_has(name).or(Player.all.name_jp_has(name))
+    end
 end
