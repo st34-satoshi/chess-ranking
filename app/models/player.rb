4821782e6f410638b953c8af3360e405c91fc248
Satoshi Tanaka <st34.satoshi@gmail.com>
2021-12-29 02:02:44 +0900

    fix: default upper bound

--- app/models/player.rb
+++ app/models/player.rb
@@ -12,6 +12,7 @@ class Player < ApplicationRecord
   def self.filtered_players(params)
     rating_lower = params[:rating_lower].to_i
     rating_upper = params[:rating_upper].to_i
+    rating_upper = 3000 unless params[:rating_upper]
     players = self.players_name_has(params[:name]).standard_rating_between(rating_lower, rating_upper)
     if params[:active]
       if params[:active] == "active"
