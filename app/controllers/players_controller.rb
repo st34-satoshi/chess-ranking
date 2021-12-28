562052b277c0475f748fe0262db0997553ada236
Satoshi Tanaka <st34.satoshi@gmail.com>
2021-12-29 01:33:06 +0900

    add: able to filter by rating

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,15 @@
 class PlayersController < ApplicationController
 
   def index
-    @players = Player.players_name_has(params[:name]).ordered.page(params[:page]).per(25)
+    @players = Player.filtered_players(params).ordered.page(params[:page]).per(25)
+    # TODO: create a Filter model
+    @name = params[:name]
+    @rating_lower = params[:rating_lower].to_i
+    if params[:rating_upper]
+      @rating_upper = params[:rating_upper].to_i
+    else
+      @rating_upper = 3000
+    end
   end
+
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,9 +1,16 @@
 class Player < ApplicationRecord
   scope :name_en_has, ->(name) { where("name_en like ?", "%#{name}%") }
   scope :name_jp_has, ->(name) { where("name_jp like ?", "%#{name}%") }
+  scope :standard_rating_between, ->(from, to) { where(rating_standard: from..to) }
   scope :ordered, -> { order(rating_standard: "DESC") }
 
   def self.players_name_has(name)
     Player.all.name_en_has(name).or(Player.all.name_jp_has(name))
+  end
+
+  def self.filtered_players(params)
+    rating_lower = params[:rating_lower].to_i
+    rating_upper = params[:rating_upper].to_i
+    self.players_name_has(params[:name]).standard_rating_between(rating_lower, rating_upper)
   end
 end
