232ca14140bd2269bf80d6e5adc732b8bae378a3
Satoshi Tanaka <st34.satoshi@gmail.com>
2022-01-08 15:03:31 +0900

    clean: use player search parameter model in a view

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -9,15 +9,6 @@ class PlayersController < ApplicationController
   def index
     @search_parameter = search_params
     @players = Player.filtered_players(@search_parameter).ordered.page(params[:page]).per(25)
-    # TODO: create a Filter model
-    @name = params[:name]
-    @rating_lower = params[:rating_lower].to_i
-    @rating_upper = if params[:rating_upper]
-                      params[:rating_upper].to_i
-                    else
-                      3000
-                    end
-    @active = params[:active] || 'all'
   end
   # rubocop:enable Metrics/AbcSize
 
