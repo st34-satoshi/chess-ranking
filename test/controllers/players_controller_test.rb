6b79765fac20ce30fd64caca9661c0533993da5b
Satoshi Tanaka <st34.satoshi@gmail.com>
2022-01-08 18:07:09 +0900

    clean: remove needless comments

--- test/controllers/players_controller_test.rb
+++ test/controllers/players_controller_test.rb
@@ -3,9 +3,6 @@
 require 'test_helper'
 
 class PlayersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
   test 'should get index' do
     get players_path
     assert_response :success
