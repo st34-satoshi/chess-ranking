0f6b223fe0ce07f74d661cfcaf409464c5c254d3
Satoshi Tanaka <st34.satoshi@gmail.com>
2022-02-03 00:00:34 +0900

    update: add locale params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    before_action :set_locale
+
+    def set_locale
+        I18n.locale = locale
+    end
+
+    def locale
+        @locale ||= params[:locale] ||= I18n.default_locale
+    end
+
+    def default_url_options(options={})
+        options.merge(locale: locale)
+    end
 end
