39a439e789a4b931799b8d1e57c36b9fbbe123e0
Satoshi Tanaka <st34.satoshi@gmail.com>
2023-10-09 14:35:33 +0900

    show chart

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "bootstrap"
+
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
+pin 'bootstrap', preload: true
+pin 'jquery', to: 'https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js', preload: true
+pin "chart.js", to: "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js", preload: true
+
 pin "application", preload: true
 
 pin 'player/show', preload: true
