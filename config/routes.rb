b37cf5a0d4736a607ea2d4f6312adb74086f64b3
Satoshi Tanaka <st34.satoshi@gmail.com>
2023-10-19 21:56:55 +0900

    add: distribution page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :players, only: %i[index show]
   get '/all_players' => 'players#index_json'
   get '/' => 'players#root'
+  get '/distribution' => 'home#distribution'
 end
