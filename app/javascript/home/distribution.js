8603b217cf7781c36385702fe0215b932266c756
Satoshi Tanaka <st34.satoshi@gmail.com>
2023-10-19 22:49:45 +0900

    tmp: hello line chart

---
+++ app/javascript/home/distribution.js
@@ -0,0 +1,31 @@
+import "application"
+import "chart.js"
+
+function createRatingDistributionGraph(){
+    const labels = [1,2,3,4,5,6,7,8,9,'a','s','d'];
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      }]
+    };
+
+    const config = {
+      type: 'line',
+      data,
+      options: {}
+    };
+
+    const ratingDistributionChart = new Chart(
+      document.getElementById('ratingDistributionChart'),
+      config
+    );
+}
+
+$(function() {
+    createRatingDistributionGraph()
+});
