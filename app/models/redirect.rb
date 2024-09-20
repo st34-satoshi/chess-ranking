57e1d22b76116856da71ff632269bae8f3c1a43f
Satoshi Tanaka <st34.satoshi@gmail.com>
2024-09-20 17:22:38 +0900

    playerが見つからないとき昔のpublic_uidを使って検索する

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -1,2 +1,8 @@
 class Redirect < ApplicationRecord
+  def self.new_player_public_uid(old_uid)
+    ncs = find_by(public_uid: old_uid)&.ncs_id
+    return nil if ncs.nil?
+
+    Player.find_by(ncs_id: ncs)&.public_uid
+  end
 end
