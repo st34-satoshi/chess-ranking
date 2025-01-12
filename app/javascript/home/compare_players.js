6e7febf52d1690a9e51c9d6e7b800aeaa9a38837
Satoshi Tanaka <st34.satoshi@gmail.com>
2025-01-12 20:42:18 +0900

    switch tab

--- app/javascript/home/compare_players.js
+++
@@ -1,1 +0,0 @@
-console.log("compare_players.js");
--- config/importmap.rb
+++ config/importmap.rb
@@ -11,4 +11,5 @@ pin 'application', preload: true
 pin 'player/show', preload: true
 pin 'home/distribution', preload: true
 pin 'home/victory_distance', preload: true
-pin 'home/compare_players', preload: true
+pin 'players_comparison/index', preload: true
+pin 'players_comparison/switch_tab', preload: true
