647763a78547c6cb662912f8bf6a3f30c4dd73ba
Satoshi Tanaka <st34.satoshi@gmail.com>
2025-01-12 20:10:51 +0900

    tmp: create players comparison

--- app/controllers/players_comparisons_controller.rb
+++ app/controllers/players_comparisons_controller.rb
@@ -4,5 +4,22 @@ class PlayersComparisonsController < ApplicationController
   end
 
   def show
+    @players_comparison = PlayersComparison.find_by!(public_uid: params[:id])
+  end
+
+  def create
+    @players_comparison = PlayersComparison.new(players_comparison_params)
+    if @players_comparison.save
+      redirect_to players_comparisons_path
+      # redirect_to players_comparison_path(@players_comparison)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def players_comparison_params
+    params.require(:players_comparison).permit(:input_text, :result_url)
   end
 end
