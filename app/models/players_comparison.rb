6d36297fdf0e748e0edd6c956e3745a0e1626e6e
Satoshi Tanaka <st34.satoshi@gmail.com>
2025-01-12 21:12:05 +0900

    save result url

--- app/models/players_comparison.rb
+++ app/models/players_comparison.rb
@@ -1,7 +1,15 @@
 class PlayersComparison < ApplicationRecord
   generate_public_uid
-  
+  before_save :clean_result_url
+
   def to_param
     public_uid
+  end
+
+  private
+
+  def clean_result_url
+    self.result_url = result_url&.strip
+    self.result_url = result_url&.split('?')&.first
   end
 end
