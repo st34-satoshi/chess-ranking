c17596fd787aea6a9a8d44a39b613575d94633ca
Satoshi Tanaka <st34.satoshi@gmail.com>
2025-01-13 18:44:21 +0900

    グラフに表示するレーティング範囲を指定できる

--- app/javascript/players_comparison/graph.js
+++ app/javascript/players_comparison/graph.js
@@ -1,29 +1,37 @@
 import "chart.js"
 
+let PlayersComparisonChart = null;
+
 function createGraph() {
-  let records = $('#playersComparisonChart').data('players-records');
+  if (PlayersComparisonChart) {
+    PlayersComparisonChart.destroy();
+  }
+  let players_records = $('#playersComparisonChart').data('players-records');
   let dates = $('#playersComparisonChart').data('dates');
-  console.log(records);
-  console.log(dates);
+  let minRating = parseInt($('#ratingMin').val());
+  let maxRating = parseInt($('#ratingMax').val());
 
   let graphData = [];
   const skipped = (ctx, value) => ctx.p0.skip || ctx.p1.skip ? value : undefined;
-  for (let i = 0; i < records.length; i++) {
+  for (let i = 0; i < players_records.length; i++) {
     const ratings = [];
+    let lastRating = 0;
     for (let j = 0; j < dates.length; j++) {
-      if (records[i]['ratings'][dates[j]]) {
-        ratings.push(records[i]['ratings'][dates[j]]);
+      if (players_records[i]['ratings'][dates[j]]) {
+        ratings.push(players_records[i]['ratings'][dates[j]]);
+        lastRating = players_records[i]['ratings'][dates[j]];
       }else{
         ratings.push(null);
       }
     }
     let data = {
-      label: records[i]['name'],
+      label: players_records[i]['name'],
       data: ratings,
       spanGaps: true,
       fill: false,
-      borderColor: `hsl(${i * 360 / records.length}, 70%, 50%)`,  // 色相を均等に分配
-      backgroundColor: `hsl(${i * 360 / records.length}, 70%, 50%)`,  // 同じ色を背景にも設定
+      hidden: lastRating < minRating || lastRating > maxRating,
+      borderColor: `hsl(${i * 360 / players_records.length}, 70%, 50%)`,  // 色相を均等に分配
+      backgroundColor: `hsl(${i * 360 / players_records.length}, 70%, 50%)`,  // 同じ色を背景にも設定
       segment: {
         borderColor: ctx => skipped(ctx, 'rgb(0,0,0,0.2)'),
         borderDash: ctx => skipped(ctx, [6, 6]),
@@ -49,11 +57,19 @@ function createGraph() {
       }
   };
 
-  var playersComparisonChart = new Chart(
+  PlayersComparisonChart = new Chart(
     document.getElementById('playersComparisonChart'),
     config
   );
 }
+
+document.getElementById('ratingMin').addEventListener('change', (e) => {
+  createGraph();
+});
+
+document.getElementById('ratingMax').addEventListener('change', (e) => {
+  createGraph();
+});
 
 document.addEventListener('DOMContentLoaded', () => {
   createGraph();
